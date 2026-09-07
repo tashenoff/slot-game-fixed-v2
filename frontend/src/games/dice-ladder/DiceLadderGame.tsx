ef84d8e5e4a83b0013f347bd6e51563e858690c3
tashenoff <kail.dead.kd@gmail.com>
2026-09-07 19:58:02 +0600

    .

--- frontend/src/games/dice-ladder/DiceLadderGame.tsx
+++ frontend/src/games/dice-ladder/DiceLadderGame.tsx
@@ -62,12 +62,9 @@ const DiceLadderGame: React.FC<DiceLadderGameProps> = ({
   };
 
   useEffect(() => {
-    document.documentElement.classList.add('in-game');
     document.body.classList.add('in-game');
     document.body.classList.remove('in-lobby');
-    window.scrollTo(0, 0);
     return () => {
-      document.documentElement.classList.remove('in-game');
       document.body.classList.remove('in-game');
     };
   }, []);
